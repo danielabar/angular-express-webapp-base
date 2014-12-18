454521c741eae5a94fc4e6e2019241ab7757cf68
Daniela Baron <dbaron@oculusinfo.com>
2014-12-18 18:27:52 -0500

    fixup logic in dao based on testing error case

--- test/lib/daoSpec.js
+++ test/lib/daoSpec.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /*jshint expr: true*/
 
 var expect = require('chai').expect;
@@ -10,9 +11,20 @@ describe('Dao', function() {
     var query = 'SELECT code, name FROM country WHERE name = $1::text';
     var params = ['Belgium'];
     var result = dao.executeQuery(query, params, function(err, result) {
-      expect(result).to.have.length(1);
-      expect(result[0].code).to.equal('BEL');
-      expect(result[0].name).to.equal('Belgium');
+      expect(err).to.be.null;
+      expect(result.rows).to.have.length(1);
+      expect(result.rows[0].code).to.equal('BEL');
+      expect(result.rows[0].name).to.equal('Belgium');
+      done();
+    });
+  });
+
+  it('Calls back with error when invalid query is specified', function(done) {
+    var invalidQuery = 'SELECT code, name FROM country WHERE foo = $1';
+    var params = ['Belgium'];
+    var result = dao.executeQuery(invalidQuery, params, function(err, result) {
+      expect(err).not.to.be.null;
+      expect(result).to.be.undefined;
       done();
     });
   });
