ad8fc177a6d525e94f26a88613054a2c4f56a345
Daniela Baron <dbaron@oculusinfo.com>
2014-12-23 10:46:43 -0500

    introduce transaction middleware

---
+++ test/lib/country_rSpec.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/*jshint expr: true*/
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var countryResource = require('../../lib/country_r');
+var db = require('../../lib/database');
+
+describe.only('Country Resource', function() {
+
+  var client;
+  beforeEach(function(done) {
+    // Get a real connection and make commit a no-op
+    db.getConnection(function(err, c) {
+      client = c;
+      client.commit = function(callback) {
+        callback(null);
+      };
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    client.rollback(done);
+  });
+
+  describe('Create', function() {
+
+    it('Success', function(done) {
+      var country = {
+        code: 'AA1',
+        name: 'Test Country 1',
+        continent: 'Europe',
+        region: 'Western Europe',
+        code2: 'AA'
+      };
+
+      // A stubbed out client with no-op commit
+      var dbStub = sinon.stub(db, 'getConnection', function(done) {
+        console.log('test returning stubbed out client');
+        done(null, client);
+      });
+
+      // TODO Put this in async waterfall
+      countryResource.create(country, function(err, insertResult) {
+        expect(err).to.be.null;
+        expect(insertResult.rowCount).to.equal(1);
+        client.query('SELECT code FROM country where code = $1', ['AA1'], function(err, result) {
+          expect(err).to.be.null;
+          expect(result.rows).to.have.length(1);
+          expect(result.rows[0].code).to.equal('AA1');
+          done();
+        });
+      });
+    });
+
+  });
+
+});
