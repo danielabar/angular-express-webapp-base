c3246d662c9d5503b64075ecaeab0657706c8494
Daniela Baron <dbaron@oculusinfo.com>
2014-12-24 10:48:51 -0500

    get by code

--- test/lib/countryApiSpec.js
+++ test/lib/countryApiSpec.js
@@ -60,4 +60,28 @@ describe('Country API', function() {
       .expect(501, done);
   });
 
+  it('GET /country/:id returns finds country by country code', function(done) {
+    var result;
+    request(app)
+      .get('/api/country/AUS')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(200);
+        result = JSON.parse(res.text);
+        expect(result[0].code).to.equal('AUS');
+        expect(result[0].name).to.equal('Australia');
+        expect(result[0].continent).to.equal('Oceania');
+        done();
+      });
+  });
+
+  it('GET /country/:id returns 404 when country code not found', function(done) {
+    var result;
+    request(app)
+      .get('/api/country/FOO')
+      .set('Accept', 'application/json')
+      .expect(404, done);
+  });
+
 });
