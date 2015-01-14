e0dc3eae66138f88b38d7e5b7a5d60c5df0a7808
Daniela Baron <dbaron@oculusinfo.com>
2015-01-14 07:38:24 -0500

    example usage of ng-grid on admin page to display list of users

--- webclient/js/controllers/admin.js
+++ webclient/js/controllers/admin.js
@@ -8,12 +8,14 @@ myapp.controller('AdminController', function($scope, UserResource) {
     // Grid Options
     $scope.gridOpts = {
       data: 'users',
-      showGroupPanel: true,
+      showGroupPanel: false,
       multiSelect: false,
       selectedItems: $scope.selected,
       columnDefs: [
+        {field: 'username', displayName: 'Username'},
         {field: 'first_name', displayName: 'First Name'},
-        {field: 'last_name', displayName: 'Last Name'}
+        {field: 'last_name', displayName: 'Last Name'},
+        {field: 'email', displayName: 'Email Address'}
       ]
     };
 
